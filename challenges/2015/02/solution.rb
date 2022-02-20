d2efaa26c4de8deaa685ad901d6813e533eed8a8
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-02-20 23:48:09 +0100

    🎁 Solve Day 02

---
+++ challenges/2015/02/solution.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Year2015
+  class Day02 < Solution
+    def part_one
+      data.map do |h, l, w|
+        2 * h * l + 2 * l * w + 2 * w * h + h * l
+      end.reduce(:+)
+    end
+
+    def part_two
+      data.map do |h, l, w|
+        h + h + l + l + h * l * w
+      end.reduce(:+)
+    end
+
+    private
+
+    def process_input(line)
+      line.split('x').map(&:to_i).sort
+    end
+  end
+end
