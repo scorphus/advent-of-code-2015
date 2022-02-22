4f0455c6bd6369db359d190ef2ed29824dd3fca2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-02-22 12:40:51 +0100

    🕯️ Reduce Branch Condition size for part_two

--- challenges/2015/03/solution.rb
+++ challenges/2015/03/solution.rb
@@ -24,17 +24,21 @@ module Year2015
 
     def part_two
       (_, _, _, _, houses) =
-        data.each_slice(2).reduce([0, 0, 0, 0, { [0, 0] => true }]) do |(sx, sy, rx, ry, houses), (sd, rd)|
-        (sdx, sdy) = @deltas[sd]
-        houses[[sx + sdx, sy + sdy]] = true
-        (rdx, rdy) = @deltas[rd]
-        houses[[rx + rdx, ry + rdy]] = true
-        [sx + sdx, sy + sdy, rx + rdx, ry + rdy, houses]
-      end
+        data.each_slice(2).reduce([0, 0, 0, 0, { [0, 0] => true }]) do |memo, obj|
+          reduce_input(memo, obj)
+        end
       houses.size
     end
 
     private
+
+    def reduce_input((sx, sy, rx, ry, houses), (sd, rd))
+      (sdx, sdy) = @deltas[sd]
+      houses[[sx + sdx, sy + sdy]] = true
+      (rdx, rdy) = @deltas[rd]
+      houses[[rx + rdx, ry + rdy]] = true
+      [sx + sdx, sy + sdy, rx + rdx, ry + rdy, houses]
+    end
 
     def process_input(line)
       line.split('')
