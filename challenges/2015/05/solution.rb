bef015be766840f994e4c9b20885875ffbc02b67
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-02-24 22:02:43 +0100

    👌 Solve Day 05

---
+++ challenges/2015/05/solution.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'set'
+
+module Year2015
+  class Day05 < Solution
+    def part_one
+      data.map do |line|
+        nice?(line) ? 1 : 0
+      end.reduce(:+)
+    end
+
+    def part_two
+      data.map do |line|
+        nicer?(line) ? 1 : 0
+      end.reduce(:+)
+    end
+
+    private
+
+    def initialize(input)
+      super
+      @vowels = Set['a'.ord, 'e'.ord, 'i'.ord, 'o'.ord, 'u'.ord]
+      @offenders = Set['b'.ord, 'd'.ord, 'q'.ord, 'y'.ord]
+    end
+
+    def nice?(line)
+      vowels_count = 0
+      dupes = false
+      prev = -1
+      line.split('').map(&:ord).each do |curr|
+        return false if @offenders.include?(curr) && curr - prev == 1
+
+        vowels_count += 1 if @vowels.include?(curr)
+        dupes = true if curr == prev
+        prev = curr
+      end
+      vowels_count >= 3 and dupes
+    end
+
+    def nicer?(line)
+      pair = letter = false
+      prev_prev = prev = -1
+      indexes = {}
+      line.split('').map(&:ord).each_with_index do |curr, i|
+        (pair, indexes) = check_pair(prev, curr, i, indexes) unless pair
+        letter = true if curr == prev_prev
+        prev_prev = prev
+        prev = curr
+        break if pair && letter
+      end
+      pair and letter
+    end
+
+    def check_pair(prev, curr, index, indexes)
+      if indexes.key?([curr, prev])
+        [indexes[[curr, prev]] < index - 1, indexes]
+      else
+        [false, indexes.merge({ [curr, prev] => index })]
+      end
+    end
+  end
+end
---
+++ spec/2015/05/solution_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2015::Day05 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2015/05/input.txt')) }
+  let(:example_part_one) do
+    <<~EXAMPLE
+      ugknbfddgicrmopn
+      aaa
+      jchzalrnumimnmhp
+      haegwjzuvuyypxyu
+      dvszwmarrgswjxmb
+    EXAMPLE
+  end
+  let(:example_part_two) do
+    <<~EXAMPLE
+      qjhvhtzxzqqjkmpb
+      xxyxx
+      uurcxstgmygtbstg
+      ieodomkazucvgmuy
+    EXAMPLE
+  end
+
+  describe 'part one' do
+    it 'finds 2 nice strings in example input' do
+      expect(described_class.part_one(example_part_one)).to eq(2)
+    end
+
+    it 'finds 236 nice strings in input' do
+      expect(described_class.part_one(input)).to eq(236)
+    end
+  end
+
+  describe 'part two' do
+    it 'finds 2 nice strings in example input' do
+      expect(described_class.part_two(example_part_two)).to eq(2)
+    end
+
+    it 'finds 51 nice strings in input' do
+      expect(described_class.part_two(input)).to eq(51)
+    end
+  end
+end
